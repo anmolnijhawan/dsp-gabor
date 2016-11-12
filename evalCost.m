03afa31552984d420958e3d7a3a244d0937d7c4d
Ameya <ameyag416@gmail.com>
2016-11-12 18:18:05 +0530

    Add files via upload

--- evalCost.m
+++ evalCost.m
@@ -5,14 +5,31 @@ DEBUG = 1;
 cost = zeros(size(population,1));
 
 for pop = 1:size(population,1)
+   if DEBUG
+       disp('evalCost: Creating gabor filter')
+       pop
+   end
    [Gr,Gi] = build_gabor_kernel(population(:,pop));
+   
    Y = [];
    C = [];
    
    for i = 1:size(in_img,3)
+       if DEBUG
+           disp('evalCost: Applying gabor filter')
+           i
+           size(Gr)
+           size(in_img(:,:,i))
+       end
        gr = imfilter(double(in_img(:,:,i)),Gr);
        gi = imfilter(double(in_img(:,:,i)),Gi);
+       if DEBUG
+           disp('evalCost: Magnitude')
+       end
        m = magnitude(gr, gi);
+       if DEBUG
+           disp('evalCost: Smoothing')
+       end
        s = smoothing(m, 5, 5);
        
        if DEBUG
@@ -28,6 +45,9 @@ for pop = 1:size(population,1)
 %            pause(5);
            
        end
+       if DEBUG
+            disp('evalCost: Scanning')
+       end
        if i == 1
             X1 = scanningwindows(s, 5, 5, 3);
             [tmpY,tmpC] = C_Y_of_X(X1);
@@ -39,14 +59,18 @@ for pop = 1:size(population,1)
        Y = [Y tmpY];
        C = [C tmpC];
    end
+   if DEBUG
+       disp('evalCost: Scattering')
+   end
    figure(4)
    scatter(X1(1,:),X1(2,:));
    hold on
    scatter(X2(1,:),X2(2,:));
    hold off
-   
-   Y
-   C
+   if DEBUG
+       Y
+       C
+   end
    d = (Y(1)-Y(2))'*(Y(1)-Y(2));
    s = max(C);
    cost(pop) = d/s;
--- scanningwindows.m
+++ scanningwindows.m
@@ -1,17 +1,24 @@
+function [ fvector ] = scanningwindows( A,m,n,s )
+% SCANNINGWINDOWS
 % A is matrix; m and n represent the window size; 
 % s represents the stride by which the widows move;
-
-function [ fvector ] = scanningwindows( A,m,n,s )
+DEBUG = 1;
 an=size(A);
-fvector = [];
+imax = length(1:s:(an(1)-m+1));
+jmax = length(1:s:(an(2)-n+1));
+fvector = zeros(2,imax*jmax);
 for i = 1:s:(an(1)-m+1)
      for j = 1:s:(an(2)-n+1)
            tmp = A(i:i+m-1,j:j+n-1);
            %mean= mean(mean(tmp));
            %variance = var(var(A,1),1);
            fvector1 = [mean(tmp(:)); var(tmp(:),1)];
-           fvector = [fvector fvector1];
+           fvector(:,(i-1)*jmax+j) = fvector1;
                
      end
-end    
+end
+if DEBUG
+       disp('scanningwindows:')
+       fvector
+end
 end
