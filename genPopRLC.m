0ae0399aac9478ffefea6f04e64fa0674ca5cf62
Ameya <ameyag416@gmail.com>
2016-11-12 22:57:33 +0530

    Runs but unsure about 'works'

--- genPopRLC.m
+++ genPopRLC.m
@@ -4,12 +4,12 @@ function [ newPopRLC ] = genPopRLC(population, sMin,sMax,nRLC )
    li=size(population);
    newPopRLC=zeros(li(1),nRLC);
    for i=1:1:nRLC
-       alpha=rand(2,1);
+       alpha=rand(3,1);
        index=[ceil(alpha(1)*li(2));ceil(alpha(2)*li(2))];
        
        index(index>li(2)) = li(2);
        index(index<1) = 1;
-       newSample=alpha(2)*population(index(1))+alpha(1)*population(index(2));
+       newSample=alpha(3)*population(index(1))+(1-alpha(3))*population(index(2));
        %[ newSample ] = boundCheck(newSample, sMin,sMax );%this is require??it will be a point in between
         newPopRLC(:,i)=newSample;
    end
