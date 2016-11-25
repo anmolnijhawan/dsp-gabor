3fabba692895ea10225fcf6ea39cddb3f94089c3
nikhilnagaraj <nikhilnagaraj.0996@gmail.com>
2016-11-25 12:00:27 +0530

    Made some changes related to loading and checking of images
    There is no predefined size for the images. A check has also been inserted in main to ensure that all the images are of the same size.

--- main.m
+++ main.m
@@ -5,11 +5,11 @@ DEBUG = 1;
 sMin = [  -1,   -1,       1,       1,  1,  1,     0,    0]';
 sMax = [   1,    1,      20,      20, 10, 10,    pi,   pi]';
 radius=[0.05, 0.05,       1,       1,0.1,0.1,   0.3,  0.3]';
-population_size = 20;
+population_size = 5;
 
-nRRI=10;
-nRLC=5;
-maxItr=10;
+nRRI=1;
+nRLC=1;
+maxItr=5;
 files = {'D51.gif', 'D96.gif'};
 num_gabor = 1;
 
@@ -17,6 +17,19 @@ if DEBUG
     disp('main: Calling stochasticSearch with params:')
     sMin,sMax,population_size,radius,nRRI,nRLC,maxItr,files    
 end
+
+%Run a check to see hat all the images have the same sizes. The size of the
+%first image is taken as a reference.
+reference_image_size = size(imread(char(files(1))));
+for file_number = 1:size(files,1)
+    if(reference_image_size ~= size(imread(char(files(file_number)))))
+        print('ERROR! ' + 'Image ' + file_number + '"s size doesn"t match the size of the first image');
+        return
+    end
+end
+
+    
+    
 
 joint_best_pop = stochasticSearch( sMin,sMax,population_size,radius,nRRI,nRLC,maxItr,files );
 filename = sprintf('population_variation_%s.mat',datestr(clock,'dd_HH_MM_SS'));
